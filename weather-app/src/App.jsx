d9787e8786ead515b0ce186f25f138c7a5ad6a81
PawelC <pawel.cacko@wecode.agency>
2026-04-16 16:09:27 +0200

    updt app

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -3,26 +3,60 @@ import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard";
 
 function App() {
-  const [weather, setWeather] = useState(null);
+    const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const key = "54343f9d4e3f2f1b987c1bd5982c9011";
+    const getWeather = async (city) => {
+        try {
+            setLoading(true);
+            setError(null);
 
-  const getWeather = async (city) => {
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=54343f9d4e3f2f1b987c1bd5982c9011&units=metric`
+            const res = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`,
+            );
+
+            const data = await res.json();
+
+            if (data.cod !== 200) {
+                setError("Nie znaleziono miasta");
+                setWeather(null);
+                return;
+            }
+
+            setWeather(data);
+        } catch (err) {
+            setError("Błąd połączenia");
+            setWeather(null);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const getBackground = () => {
+        if (!weather) return "default";
+
+        const condition = weather.weather[0].main;
+
+        if (condition === "Clear") return "clear";
+        if (condition === "Clouds") return "clouds";
+        if (condition === "Rain") return "rain";
+
+        return "default";
+    };
+
+    return (
+        <div className={`app ${getBackground()}`}>
+            <h1>🌤 Weather App</h1>
+            <SearchBar onSearch={getWeather} />
+
+            {loading && <p>Ładowanie...</p>}
+
+            {error && <p className="error">{error}</p>}
+
+            {!loading && !error && weather && <WeatherCard weather={weather} />}
+        </div>
     );
-
-    const data = await res.json();
-    setWeather(data);
-  };
-
-  return (
-    <div className="app">
-      <h1>🌤 Weather App</h1>
-
-      <SearchBar onSearch={getWeather} />
-
-      {weather && <WeatherCard weather={weather} />}
-    </div>
-  );
 }
 
 export default App;
--- weather-app/src/components/WeatherCard.jsx
+++ weather-app/src/components/WeatherCard.jsx
@@ -1,7 +1,14 @@
 export default function WeatherCard({ weather }) {
+  const icon = weather.weather[0].icon;
+
   return (
     <div className="card">
       <h2>{weather.name}</h2>
+
+      <img
+        src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+        alt="icon"
+      />
 
       <h1>{Math.round(weather.main.temp)}°C</h1>
 
